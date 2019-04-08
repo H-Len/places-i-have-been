4b7b473584ce2ba16029b8236dca3a7d9d37421d
hannah melendy and Katlin Anderson <katlin@>
2019-04-08 10:15:48 -0700

    add Place constructor and prototype methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 //business logic for places I've been
+function TravelList() {
+  this.places = [];
+  this.currentId = 0;
+}
+
+TravelList.prototype.addPlace = function(place) {
+  this.places.push(place);
+}
+
+TravelList.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
 
 function Place(city, country, date) {
   this.city = city;
